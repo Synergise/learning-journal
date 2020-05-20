b64d6619091a54e068c9f0b1f92cc4f79a3c3d3e
Jason <jasylwong@gmail.com>
2020-05-20 22:51:12 +0100

    remove username requirements

--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -8,7 +8,6 @@ describe 'registration' do
       visit "/"
       click_link "Sign up"
       fill_in "Email", with: user.email
-      # fill_in "Username", with: user.username
       fill_in "Password", with: user.password
       fill_in "Password confirmation", with: user.password_confirmation 
       click_button "Sign up"
