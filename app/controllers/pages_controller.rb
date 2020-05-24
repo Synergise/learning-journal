01796073379c545027d108832628378867b44e48
Jason <jasylwong@gmail.com>
2020-05-24 23:00:52 +0100

    Address CodeClimate issues for PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Responsible for the different pages a user can access
 class PagesController < ApplicationController
   def home; end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 # Responsible for helper functions for pages
+
 module PagesHelper
 end
