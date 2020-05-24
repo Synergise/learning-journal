2685388c5d9b73bb6da644c00a9e64dbd9581626
Jason <jasylwong@gmail.com>
2020-05-24 22:27:31 +0100

    remove hardcoded emails, and tighten feature test terminology

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,7 @@
 FactoryBot.define do
-  factory :user do
-    email { 'hal_jordan@gmail.com' }
-    password { 'greenlanterncorps' }
-    password_confirmation { 'greenlanterncorps' }
-  end
-
-  factory :random_user, class: User do
+  factory :user, class: User do
     email { Faker::Internet.safe_email }
-    password { Faker::Internet.password }
-    password_confirmation { Faker::Internet.password }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
