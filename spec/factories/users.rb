403adff146b2a47e626f0073e9159853acabad66
Jason <jasylwong@gmail.com>
2020-05-26 23:02:34 +0100

    Use Faker for password

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user, class: User do
     email { Faker::Internet.safe_email }
-    password { "password" }
-    password_confirmation { "password" }
+    password { Faker::Internet.password }
+    password_confirmation { password }
   end
 end
