6cac5ec96239a7a5d8ee6d3ecbe9cd43fab81e48
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2020-05-28 09:00:52 +0100

    Add magic comments and other Rubocop fixes

--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-feature 'User registration' do
+RSpec.feature 'User registration' do
   context 'user can sign up' do
     let(:user) { build(:user) }
 
     scenario 'with valid credentials will redirect the user to the home page' do
-      visit "/"
-      click_link "Sign up"
-      fill_in "Email", with: user.email
-      fill_in "Password", with: user.password
-      fill_in "Password confirmation", with: user.password_confirmation 
-      click_button "Sign up"
+      visit '/'
+      click_link 'Sign up'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      fill_in 'Password confirmation', with: user.password_confirmation
+      click_button 'Sign up'
       expect(page).to have_content "Welcome #{user.email}!"
     end
   end
