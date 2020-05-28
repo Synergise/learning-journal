b85c26ccbe184b69e6f7675e356f47a3ea232889
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2020-05-28 14:39:00 +0100

    Add Database to GHA Pipeline, Fixes #26 (#28)
    * Add postgres service to pipeline
    
    * Create User smoke test and fix DB Cleaner
    
    * Add healthcheck and steps to prepare DB
    
    * Add POSTGRES_PASSWORD
    
    * Remove debug flag from CC
    
    * Add newline to EOF

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,18 +2,18 @@
 
 RSpec.configure do |config|
   config.before(:suite) do
-    Database.Cleaner.clean_with :truncation
+    DatabaseCleaner.clean_with :truncation
   end
 
   config.before(:each) do
-    Database.Cleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each) do
-    Database.Cleaner.start
+    DatabaseCleaner.start
   end
 
   config.before(:each) do
-    Database.Cleaner.clean
+    DatabaseCleaner.clean
   end
 end
