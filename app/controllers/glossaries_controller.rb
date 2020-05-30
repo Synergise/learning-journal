a16fb27ac8f5249013788dc76c32b5c0ed89a2d2
Jason <jasylwong@gmail.com>
2020-05-30 22:16:49 +0100

    generate glossaries controller and add routes

---
+++ app/controllers/glossaries_controller.rb
@@ -0,0 +1,30 @@
+class GlossariesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+  
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_params
+  end
+  
+  def glossary_params
+  end
+end
