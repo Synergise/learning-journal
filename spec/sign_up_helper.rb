63b63fa72304ef5b79f45072f93eea218d9b7ce7
Jason <jasylwong@gmail.com>
2020-06-05 21:48:30 +0100

    extract detail provision out for codeclimate

--- spec/sign_up_helper.rb
+++ spec/sign_up_helper.rb
@@ -3,8 +3,12 @@
 def sign_up(user)
   visit '/'
   click_link 'Sign up'
+  provide_details(user)
+  click_button 'Sign up'
+end
+
+def provide_details(user)
   fill_in 'Email', with: user.email
   fill_in 'Password', with: user.password
   fill_in 'Password confirmation', with: user.password_confirmation
-  click_button 'Sign up'
 end
