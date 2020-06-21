9fbdce33487993fb48138901b4a22245b87cc8fe
Jason <jasylwong@gmail.com>
2020-06-21 20:58:14 +0100

    change error display to error_panel partial, and remove glossary_helper

--- app/helpers/glossary_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-#:nodoc:
-module GlossaryHelper
-  def render_errors(record)
-    errors_to_render = record.errors
-    errors_to_render.messages.any? ? errors_to_render.full_messages : []
-  end
-end
