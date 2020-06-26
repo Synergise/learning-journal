3afdb70edf4456ad5050d40fbf00a628a28e0091
Jason <jasylwong@gmail.com>
2020-06-26 22:40:38 +0100

    create and pass user modfication unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user_mock) { build(:user) }
+  let!(:user_mock2) { create(:user) }
+  let(:updated_user) do
+    create(
+      :user,
+      email: user_mock.email,
+      password: user_mock.password
+    )
+  end
 
-  context "creation" do
+  context 'creation' do
     it 'will mark an empty user instance as invalid' do
       user = User.new
       user.valid?
@@ -54,6 +62,49 @@ RSpec.describe User, type: :model do
       expect(user2.errors.details[:email]).to include(
         a_hash_including(error: :taken)
       )
+    end
+  end
+
+  context 'modification' do
+    it 'removes entries from the DB upon deletion' do
+      expect { user_mock2.delete }.to change(User, :count).by(-1)
+    end
+
+    it 'will only alter the email in the DB when updated in the model' do
+      updated_user.update(email: user_mock2.email)
+      expect(updated_user.email).not_to eq(user_mock.email)
+      expect(updated_user.email).to eq(user_mock2.email)
+      expect(updated_user.password).to eq(user_mock.password)
+      expect(updated_user.password).not_to eq(user_mock2.password)
+    end
+
+    it 'will only alter the password in the DB when updated in the model' do
+      updated_user.update(password: user_mock2.password)
+      expect(updated_user.email).to eq(user_mock.email)
+      expect(updated_user.email).not_to eq(user_mock2.email)
+      expect(updated_user.password).not_to eq(user_mock.password)
+      expect(updated_user.password).to eq(user_mock2.password)
+    end
+
+    it 'updates entries in the DB upon valid editing' do
+      updated_user.update(
+        email: user_mock2.email,
+        password: user_mock2.password
+      )
+      expect(updated_user.email).not_to eq(user_mock.email)
+      expect(updated_user.email).to eq(user_mock2.email)
+      expect(updated_user.password).not_to eq(user_mock.password)
+      expect(updated_user.password).to eq(user_mock2.password)
+    end
+
+    it 'does not change DB count upon successful editing' do
+      expect do
+        user_mock2.update(
+          email: user_mock.email,
+          password: user_mock.password
+        )
+      end
+        .not_to change(User, :count)
     end
   end
 end
