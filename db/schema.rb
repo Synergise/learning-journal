e79cad58a0b02c79a63452909f434465f7f75a24
Jason <jasylwong@gmail.com>
2020-07-13 22:39:05 +0100

    add migration for labels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_210801) do
+ActiveRecord::Schema.define(version: 2020_07_13_213359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_05_30_210801) do
   create_table "glossaries", force: :cascade do |t|
     t.string "term", default: "", null: false
     t.text "definition", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
